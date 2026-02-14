59555dbaceaaad223bcd04ba4c7955cc4ca706f0
bhatramchandra91-gif <bhatramchandra91@gmail.com>
2026-02-14 13:31:00 +0530

    Delete crm_store.py

--- crm_store.py
+++
@@ -1,8 +0,0 @@
-clients = []
-
-def add_client(name, business):
-    clients.append({"name": name, "business": business})
-    return {"message": "Client added"}
-
-def get_clients():
-    return clients
